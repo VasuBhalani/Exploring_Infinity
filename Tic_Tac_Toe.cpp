fdc4485737dabde7a7b89adcd54b6c41e472c759
ronit22cs031 <120238067+ronit22cs031@users.noreply.github.com>
2023-04-28 21:05:02 +0530

    Created design function for Tic Tac Toe game

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+#include<stdlib.h>
 
+using namespace std;
+
+char board[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+void design(){
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+    cout<<"   "<<board[0]<<"   |   "<<board[1]<<"   |   "<< board[2]<<endl;
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+    cout<<"------- ------- -------"<<endl;
+
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+    cout<<"   "<<board[3]<<"   |   "<<board[4]<<"   |   "<< board[5]<<endl;
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+    cout<<"------- ------- -------"<<endl;
+
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+    cout<<"   "<<board[6]<<"   |   "<<board[7]<<"   |   "<< board[8]<<endl;
+    cout<<"   "<<"    |   "<<"    |   "<<endl;
+
+}
